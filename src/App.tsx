51eaed779ffa574c5238ba172ad49ed967052643
Mykola Stanzh <149831982+Mykolyan@users.noreply.github.com>
2026-03-23 15:43:51 +0200

    implemented users load from API, UserSelector component using users data from API, instead of hardCoded users in dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,77 @@
-import classNames from 'classnames';
-
+import { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 
-import { PostsList } from './components/PostsList';
-import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
-import { Loader } from './components/Loader';
+import { User } from './types/User';
+import { getUsers } from './api/users';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [usersError, setUsersError] = useState(false);
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+  useEffect(() => {
+    setIsLoadingUsers(true);
+    setUsersError(false);
 
-              <Loader />
+    getUsers()
+      .then(setUsers)
+      .catch(() => setUsersError(true))
+      .finally(() => setIsLoadingUsers(false));
+  }, []);
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  users={users}
+                  selectedUser={selectedUser}
+                  onUserSelect={setSelectedUser}
+                  isLoading={isLoadingUsers}
+                />
+
+                {usersError && (
+                  <p className="has-text-danger">Failed to load users</p>
+                )}
               </div>
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
+              <div className="block" data-cy="MainContent">
+                {!selectedUser && (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                )}
 
-              <PostsList />
+                {selectedUser && (
+                  <div className="notification is-warning" data-cy="NoPostsYet">
+                    No posts yet
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
-          </div>
+          {/* <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              'Sidebar--open',
+            )}
+          >
+            <div className="tile is-child box is-success ">
+              <PostDetails />
+            </div>
+          </div> */}
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
